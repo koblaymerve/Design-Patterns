1c79f64077fa27966965c396c471a4be200af7a0
Merve Koblay <koblaymrv@gmail.com>
2019-04-03 22:48:35 +0300

    decorator design pattern

---
+++ Decorator/CarBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    abstract class CarBase
+    {
+        public string Make { get; set; }
+    }
+}
