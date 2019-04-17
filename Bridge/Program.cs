58ecba0521c49c2d7e056b99d00072875ba9b20a
Merve Koblay <koblaymrv@gmail.com>
2019-04-17 22:50:52 +0300

    Bridge Design Pattern

---
+++ Bridge/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.MessageSenderBase = new EmailSender();
+            customerManager.UpdateCustomer();
+            Console.ReadLine();
+        }
+    }
+
+
+    abstract class MessageSenderBase
+    {
+        public void Save()
+        {
+            Console.WriteLine("Message Saved!");
+        }
+
+        public abstract void Send(Body body);
+    }
+
+    public class Body
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+    class SmsSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine($"{body} was send via sms sender!");
+        }
+    }
+
+    class EmailSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine($"{body} was send via email sender!");
+        }
+    }
+
+
+    class CustomerManager
+    {
+        public MessageSenderBase MessageSenderBase { get; set; }
+        public void UpdateCustomer()
+        {
+            MessageSenderBase.Send(new Body{ Title = "About the course"});
+            Console.WriteLine("Customer updated!");
+        }
+
+    }
+}
